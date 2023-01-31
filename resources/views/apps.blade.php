471aba6aad1248dbc093c193cf1d3fa6ffc1c3dc
Juan Oliva <joliva@laanonima.com.ar>
2023-01-31 12:05:33 -0300

    cambios esteticos

--- resources/views/apps.blade.php
+++ resources/views/apps.blade.php
@@ -1,20 +1,24 @@
 @extends('welcome')
 
 @section('header')
-<header class="w-100 text-center">
-    <h1 class="fw-bold">Apps</h1>
+<header class="w-100 text-center mb-4 mt-2">
+    <h1 class="fw-bold">APPS</h1>
 </header>
 @endsection
 
 @section('seccion')
-@php
-xdebug_break()
-@endphp
-@foreach($apps as $app)
-<a class="btn col-12 col-lg-5 bg-light text-left cursor-pointer py-3" href="{{route($app['name'])}}">
-    <div class="card-header">
-        <h5 class="card-title">{{ $app['name'] }}</h5>
-    </div>
-</a>
-@endforeach
+<div class="container-fluid d-flex flex-column flex-md-row justify-content-md-center">
+    @php
+    xdebug_break()
+    @endphp
+    @foreach($apps as $app)
+    <a class="btn btn-dark col-12 col-md-5 text-left cursor-pointer py-3 text-uppercase mt-2 mt-md-0 mx-md-3"
+        href="{{route($app['name'])}}">
+        <div class="card-header">
+            <h5 class="card-title">{{ $app['name'] }}</h5>
+        </div>
+    </a>
+
+    @endforeach
+</div>
 @endsection
